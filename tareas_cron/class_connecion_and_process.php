da4ad06b1f5b806d3545b5b3dac910e043b290bb
barcoMarquez <pablob.marquez@hotmail.com>
2021-06-04 18:37:59 -0500

    credenciales crontap php

--- tareas_cron/class_connecion_and_process.php
+++ tareas_cron/class_connecion_and_process.php
@@ -12,7 +12,8 @@ class db_and_procesos{
     }
 
     function Connection(){
-        if(md5($_SERVER['SERVER_NAME'])=='068234a2d85a5233fd17f6d0507d3454'){
+
+        /*if(md5($_SERVER['SERVER_NAME'])=='068234a2d85a5233fd17f6d0507d3454'){
             #REMOTO
             $conexion = null;
             $host     = 'localhost'; #ip o nombre del servidor remoto o local
@@ -28,7 +29,16 @@ class db_and_procesos{
             $username = 'root';
             $password = ''; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
             $utf8mb4  = 'utf8mb4';
-        }
+        }*/
+
+        #REMOTO
+        $conexion = null;
+        $host     = 'localhost'; #ip o nombre del servidor remoto o local
+        $database = 'adminnub_sch_dental_entity_login'; //SE ENCUENTRA TODAS LAS ENTIDADES REGISTRADAS
+        $username = 'adminnub_entidad_dental'; //Usuario de la Base de datos todos los privilegios
+        $password = '740631f8cd06c9b56f1190b29db9ec54'; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
+        $utf8mb4  = 'utf8mb4';
+
         try{
             $conexion = new PDO("mysql:host=$host;dbname=$database;charset=$utf8mb4",$username, $password );
         }catch (PDOException $e){
