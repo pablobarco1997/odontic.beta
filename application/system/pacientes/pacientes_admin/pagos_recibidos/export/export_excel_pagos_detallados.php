a0b509c54f0ceb1e17cab0e2480935e85670dbb6
barcoMarquez <pablob.marquez@hotmail.com>
2021-10-18 16:41:50 -0500

    exports pagos recibidor por pacientes

---
+++ application/system/pacientes/pacientes_admin/pagos_recibidos/export/export_excel_pagos_detallados.php
@@ -0,0 +1,230 @@
+<?php
+
+
+require_once '../../../../../config/lib.global.php';
+session_start();
+
+if(!isset($_SESSION['is_open'])){
+    header("location:".DOL_HTTP."/application/system/login");
+}
+
+error_reporting(E_ALL);
+
+define('PC_MEMORY_LIMIT', '1024M');
+ini_set('memory_limit',PC_MEMORY_LIMIT);
+set_time_limit(20000);
+
+
+require_once  DOL_DOCUMENT .'/application/system/conneccion/conneccion.php';    //Coneccion de Empresa
+require_once  DOL_DOCUMENT .'/application/controllers/controller.php';
+require_once  DOL_DOCUMENT . '/public/lib/PHPExcel2014/PHPExcel.php';
+
+$objPHPExcel = new PHPExcel();
+
+
+
+
+function aplicarColunmDetalle($titulos = array(), $detallesRows = array(), $rows, $objPHPExcel, $marcelTitle=false){
+
+    $acuRowis = $rows;
+    foreach ($titulos as $col => $item){
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValueByColumnAndRow($col, $acuRowis, $item);
+    }
+
+//    $last = $objPHPExcel->getActiveSheet()->getHighestDataColumn(); //ultimo Columna en uso
+    $last = $objPHPExcel->setActiveSheetIndex(0)->getColumnDimensionByColumn($col)->getColumnIndex(PHPExcel_Worksheet::BREAK_COLUMN);
+
+    if($marcelTitle==true){
+//        print_r($lastColumn); die();
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('A'.$acuRowis.':'.$objPHPExcel->getActiveSheet()->getHighestDataColumn().$acuRowis);
+        $fill = array(
+            'type' => PHPExcel_Style_Fill::FILL_SOLID,
+            'color' => array(
+                'argb' => 'dbeef3'));
+        $alignment = array();
+        $borders = array();
+    }else{
+        $fill = array();
+        $alignment = array( 'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,'wrap' => TRUE);
+        $borders = array('bottom'=>array('style'=>PHPExcel_Style_Border::BORDER_DOUBLE));
+    }
+
+
+    $objPHPExcel->getActiveSheet()->getStyle('A'.$acuRowis.':'.$last.$acuRowis)->applyFromArray(
+        array(
+            'font' => array(
+                'bold' => true,
+                'size' => 12,
+            ),
+            'borders' => $borders,
+            'alignment' => $alignment,
+            'fill' => $fill
+        )
+    );
+
+    $acuRowis++; //agrego el detalle
+    foreach ($detallesRows as $k => $itemValue){
+        $col=0;
+        foreach ($itemValue as $value){
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValueByColumnAndRow($col, $acuRowis, $value);
+            $col++;
+        }
+        $acuRowis++;
+    }
+
+    return $acuRowis;
+}
+
+
+
+/**SE CREA LAS VARIABLES DE INICIO**/
+$cn = new ObtenerConexiondb();                    //Conexion global Empresa Fija
+$db = $cn::conectarEmpresa($_SESSION['db_name']); //coneccion de la empresa variable global
+
+
+$fechaData =  array();
+$sql_data = "select 
+    pddc.rowid as n_pago,
+    concat(ttp.numero_ttp,' Paciente: ',concat(adp.nombre,' ',adp.apellido)) as tratamiento,
+    pdd.feche_create as emitido,
+    prs.descripcion as servicio,
+    if(ttp.pieza=0,'',ttp.pieza) as pieza, 
+    ttp.totalttp as total,
+    sum(round(pdd.amount, 2)) as abonado,
+    round( (ttp.totalttp - sum(round(pdd.amount, 2)) ), 2) as pendiente, 
+    case 	
+		when sum(round(pdd.amount, 2))=ttp.totalttp then 'Recaudación Completa'
+        when sum(round(pdd.amount, 2))>0 && sum(round(pdd.amount, 2))<>ttp.totalttp then 'Abonado'
+        else 'Saldo Pendiente'
+        end
+        as recaudacion_estado
+        
+	, case ttp.estadodet
+		when 'A' then 'PENDIENTE'
+		when 'P' then 'EN PROCESO'
+		when 'R' then 'REALIZADO'
+		end  as estado_ttp , 
+        
+	pddc.n_fact_boleta as boleta,
+	b.nom as typebank,
+	ttp.idttp
+		
+from 
+	tab_pagos_independ_pacientes_det pdd 
+		left join
+	tab_pagos_independ_pacientes_cab pddc on pdd.fk_pago_cab = pddc.rowid
+	    inner join
+	tab_bank_operacion b on b.rowid = pddc.fk_tipopago
+		left join 
+	(select concat('Plan de Tratamiento N.',tc.numero) as numero_ttp , td.fk_diente as pieza, tc.rowid idttp , td.rowid as idttpd, sum(round(td.total, 2)) as totalttp , td.estadodet from tab_plan_tratamiento_cab tc inner join tab_plan_tratamiento_det td on td.fk_plantratam_cab = tc.rowid group by tc.rowid, td.rowid) as ttp on ttp.idttp = pdd.fk_plantram_cab and ttp.idttpd = pdd.fk_plantram_det
+		inner join 
+	tab_conf_prestaciones prs on prs.rowid = pdd.fk_prestacion
+		inner join 
+	tab_admin_pacientes adp on adp.rowid  = pdd.fk_paciente
+  WHERE pdd.fk_paciente = ".GETPOST('idpaciente');
+
+if(GETPOST('n_pago')!=''){
+    $sql_data .= " and pddc.rowid like '%".GETPOST('n_pago')."%' ";
+}
+if(GETPOST('n_x_documento')!=''){
+    $sql_data .= " and pddc.n_fact_boleta like '%".GETPOST('n_x_documento')."%' ";
+}
+if(GETPOST('formapago')!=''){
+    $sql_data .= " and b.rowid = '".GETPOST('formapago')."' ";
+}
+
+$sql_data .= " group by pdd.fk_plantram_cab, pdd.fk_plantram_det, pdd.rowid ";
+
+//print_r($sql_data); die();
+
+$data = array();
+$result = $db->query($sql_data);
+if($result){
+    if($result->rowCount()>0){
+        $data = $result->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($data as $k => $value){
+
+            $fechaData[$value['idttp']]['text'] = $value['tratamiento'];
+            $fechaData[$value['idttp']]['detalle'][] = array(
+                'emitido'            => $value['emitido'],
+                'n_pago'             => 'P_'.str_pad($value['n_pago'],6,'0',STR_PAD_LEFT),
+                'servicio'           => $value['servicio'],
+                'pieza'              => $value['pieza'],
+                'total'              => $value['total'],
+                'abonado'            => $value['abonado'],
+                'pendiente'          => $value['pendiente'],
+                'recaudacion_estado' => $value['recaudacion_estado'],
+                'estado_ttp'         => $value['estado_ttp'],
+                'typebank'           => $value['typebank'],
+                'boleta'             => $value['boleta'],
+            );
+        }
+    }else{
+        print  '<h3>No hay datos</h3>';
+        die();
+    }
+}else{
+    print  '<h3>No hay datos Ocurrio un error</h3>';
+    die();
+}
+
+
+//echo '<pre>'; print_r($fechaData); die();
+
+
+$Titulos = array('Emitido','N.Pagos','Prestación/Servicios', 'Pieza','Total','Abonado','Pendiente','Recaudación Estado','Tratamiento Estado','Forma de pago', 'Boleta');
+
+$objPHPExcel->setActiveSheetIndex(0)->setCellValueExplicit('A1', "PAGOS DETALLADOS ".date("Y/m/d"));
+$objPHPExcel->setActiveSheetIndex(0)->mergeCells('A1:K1');
+$objPHPExcel->getActiveSheet()->getStyle('A1:K1')->applyFromArray(
+    array('font' => array(  'bold' => true, 'size' => 14,
+        'color' => array(
+            'argb' => '1f497d'
+        ),
+    ),
+        'fill' => array(
+            'type' => PHPExcel_Style_Fill::FILL_SOLID,
+            'color' => array(
+                'argb' => 'dae4f0')),
+        'alignment' => array(
+            'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+            'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,),)
+);
+
+
+
+$rowsExcel = 2;
+$rowsExcel = aplicarColunmDetalle($Titulos, array(), $rowsExcel, $objPHPExcel, false);
+
+foreach ($fechaData as $k => $item_a){
+    //titulo
+    $rowsExcel = aplicarColunmDetalle(array($item_a['text']), $item_a['detalle'], $rowsExcel, $objPHPExcel, true);
+}
+
+foreach(range('A','J') as $columnID) {
+    $objPHPExcel->getActiveSheet()->getColumnDimension($columnID)
+        ->setAutoSize(true);
+}
+
+$objPHPExcel->getActiveSheet()->calculateColumnWidths();
+
+// Se asigna el nombre a la hoja
+$objPHPExcel->getActiveSheet()->setTitle('PAGOS DETALLADOS');
+// Se activa la hoja para que sea la que se muestre cuando el archivo se abre
+$objPHPExcel->setActiveSheetIndex(0);
+
+
+
+// Se manda el archivo al navegador web, con el nombre que se indica, en formato 2007
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="PAGOS DETALLADOS.xlsx"');
+header('Cache-Control: max-age=0');
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('php://output');
+
+
+
+
+
+?>
---
+++ application/system/pacientes/pacientes_admin/pagos_recibidos/export/export_excel_recaudaciones_realizadas.php
@@ -0,0 +1,195 @@
+<?php
+
+
+require_once '../../../../../config/lib.global.php';
+session_start();
+
+if(!isset($_SESSION['is_open'])){
+    header("location:".DOL_HTTP."/application/system/login");
+}
+
+error_reporting(E_ALL);
+
+define('PC_MEMORY_LIMIT', '1024M');
+ini_set('memory_limit',PC_MEMORY_LIMIT);
+set_time_limit(20000);
+
+
+require_once  DOL_DOCUMENT .'/application/system/conneccion/conneccion.php';    //Coneccion de Empresa
+require_once  DOL_DOCUMENT .'/application/controllers/controller.php';
+require_once DOL_DOCUMENT . '/public/lib/PHPExcel2014/PHPExcel.php';
+
+/**SE CREA LAS VARIABLES DE INICIO**/
+$cn = new ObtenerConexiondb();                    //Conexion global Empresa Fija
+$db = $cn::conectarEmpresa($_SESSION['db_name']); //coneccion de la empresa variable global
+
+$idplantratamiento = GETPOST('idplantratamiento');
+
+if(empty($idplantratamiento)){
+
+    echo '<h2>Error </h2>';
+    die();
+}
+
+$paciente_nomb = $db->query("select  (select concat(a.nombre, ' ', a.apellido) as nom from tab_admin_pacientes a where a.rowid = ct.fk_paciente) as paciente
+ from tab_plan_tratamiento_cab ct where rowid = $idplantratamiento")->fetchObject()->paciente;
+
+$n_tratamiento = $db->query("select concat('Plan de Tratamiento N. ', c.numero) as nom from tab_plan_tratamiento_cab c where c.rowid = $idplantratamiento")->fetchObject()->nom;
+
+$sql = "SELECT 
+    cast(pc.fecha as date) as date, 
+	concat('Plan de Tratamiento: ',' ',td.fk_plantratam_cab) as n_tratamiento,
+	p.descripcion as prestacion, 
+	if(td.fk_diente!=0,concat('Pieza: ',td.fk_diente),'') as pieza, 
+	round(td.total,2) as total_prestacion, 
+    round(sum(pd.amount),2)  abonado , 
+    if(td.estadodet = 'A', 'Pendiente', if(td.estadodet = 'P' , 'En Proceso', if(td.estadodet = 'R' , 'Realizada' , ''))) as estado, 
+    if(round(td.total,2)>round(sum(pd.amount),2), (round(td.total,2)-round(sum(pd.amount),2)),0) as  pendiente,
+    (select t.nom from tab_bank_operacion t where t.rowid = pc.fk_tipopago) as tipo_p
+FROM
+tab_plan_tratamiento_det td , 
+tab_pagos_independ_pacientes_det pd, 
+tab_pagos_independ_pacientes_cab pc, 
+tab_conf_prestaciones p
+where 
+td.rowid = pd.fk_plantram_det
+and p.rowid = pd.fk_prestacion
+and pc.rowid = pd.fk_pago_cab
+and pd.estado = 'A' ";
+
+if(!empty($idplantratamiento)){
+    $sql .= " and td.fk_plantratam_cab = ".$idplantratamiento;
+}
+
+$sql .= " group by pd.fk_prestacion, pd.fk_plantram_cab, td.fk_diente ";
+$result = $db->query($sql);
+$data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+//echo '<pre>'; print_r($data); die();
+
+
+//esta funcion me arma la tabla
+function aplicarColunmDetalle($titulos = array(), $detallesRows = array(), $rows, $objPHPExcel){
+
+    $acuRowis = $rows;
+    foreach ($titulos as $col => $item){
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValueByColumnAndRow($col, $acuRowis, $item);
+    }
+
+//    $last = $objPHPExcel->getActiveSheet()->getHighestDataColumn(); //ultimo letra
+    $last = $objPHPExcel->setActiveSheetIndex(0)->getColumnDimensionByColumn($col)->getColumnIndex(PHPExcel_Worksheet::BREAK_COLUMN);
+//    print_r($last); die();
+    $objPHPExcel->getActiveSheet()->getStyle('A'.$acuRowis.':'.$last.$acuRowis)->applyFromArray(
+        array(
+            'font' => array(
+                'bold' => true,
+                'size' => 12,
+            ),
+            'borders' => array('bottom'=>array('style'=>PHPExcel_Style_Border::BORDER_DOUBLE)),
+            'alignment' => array( 'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,'wrap' => TRUE),
+        )
+    );
+
+    $acuRowis++; //agrego el detalle
+    foreach ($detallesRows as $k => $itemValue){
+        $col=0;
+        foreach ($itemValue as $value){
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValueByColumnAndRow($col, $acuRowis, $value);
+            $col++;
+        }
+        $acuRowis++;
+    }
+
+    return $acuRowis;
+}
+
+
+
+/** PHP EXCEL **/
+
+
+$objPHPExcel = new PHPExcel();
+
+
+//titulo principal
+$objPHPExcel->setActiveSheetIndex(0)->setCellValueExplicit('A1', 'DETALLES DE PAGOS REALIZADOS DEL '.strtoupper($n_tratamiento)." ".date("Y/m/d"));
+$objPHPExcel->setActiveSheetIndex(0)->mergeCells('A1:H1');
+$objPHPExcel->getActiveSheet()->getStyle('A1:H1')->applyFromArray(
+    array('font' => array(  'bold' => true, 'size' => 14,
+            'color' => array(
+                'argb' => '1f497d'
+            ),
+        ),
+        'fill' => array(
+            'type' => PHPExcel_Style_Fill::FILL_SOLID,
+            'color' => array(
+                'argb' => 'dae4f0')),
+        'alignment' => array(
+            'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+            'vertical' => PHPExcel_Style_Alignment::VERTICAL_CENTER,),)
+);
+
+//pacientes
+$objPHPExcel->setActiveSheetIndex(0)->setCellValueExplicit('A2', 'Paciente: ');
+$objPHPExcel->setActiveSheetIndex(0)->setCellValueExplicit('B2', $paciente_nomb);
+
+$objPHPExcel->getActiveSheet()->getStyle('A2:A2')->applyFromArray(
+    array('font' => array(  'bold' => true, 'size' => 12,
+        'color' => array(
+            'argb' => '1f497d'
+        ),
+    ),)
+);
+$objPHPExcel->getActiveSheet()->getStyle('B2:B2')->applyFromArray(
+    array('font' => array(  'bold' => false, 'size' => 12,
+        'color' => array(
+            'argb' => '1f497d'
+        ),
+    ),)
+);
+
+
+$titulos_array = array('Emitido', 'Prestación/Servicios', 'Pieza', 'Total', 'Abonado', 'Pendiente', 'Forma de Pago', 'Estado');
+$detallesRow = [];
+foreach ($data as $k => $value){
+
+    $rows = [];
+        $rows[] = date('Y/m/d', strtotime($value['date']));
+        $rows[] = $value['prestacion'];
+        $rows[] = $value['pieza'];
+        $rows[] = $value['total_prestacion'];
+        $rows[] = $value['abonado'];
+        $rows[] = $value['pendiente'];
+        $rows[] = $value['tipo_p'];
+        $rows[] = $value['estado'];
+    $detallesRow[] = $rows;
+}
+
+$rowsFila = aplicarColunmDetalle($titulos_array, $detallesRow , 3, $objPHPExcel);
+
+foreach(range('A','F') as $columnID) {
+    $objPHPExcel->getActiveSheet()->getColumnDimension($columnID)
+        ->setAutoSize(true);
+}
+
+$objPHPExcel->getActiveSheet()->calculateColumnWidths();
+
+// Se asigna el nombre a la hoja
+$objPHPExcel->getActiveSheet()->setTitle('DETALLES DE PAGOS REALIZADOS');
+// Se activa la hoja para que sea la que se muestre cuando el archivo se abre
+$objPHPExcel->setActiveSheetIndex(0);
+
+
+// Se manda el archivo al navegador web, con el nombre que se indica, en formato 2007
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="Detalles de Pagos Realizados.xlsx"');
+header('Cache-Control: max-age=0');
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save('php://output');
+
+
+
+
+
+?>
